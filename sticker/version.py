cd4e5dc615d2a270122e004586cb2766924176be
dominikoso <root@dominikoso.me>
2025-03-07 21:03:55 +0000

    Update

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '9b5f2333'
+version = '0.1.0+dev.9b5f2333'
+linkified_version = '0.1.0+dev.[9b5f2333](https://github.com/maunium/stickerpicker/commit/9b5f2333311b6b0e62262cd24cb188c2b1501727)'
